f6f9f43a47cdf2a759b3b3684119402a00ac0be6
unknown <greg@mycunningham.com>
2023-05-10 16:29:14 -0500

    get newest

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const clothingItems = require("./routes/clothingItems");
 const users = require("./routes/user");
 const { NOT_FOUND } = require("./utils/errors");
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
